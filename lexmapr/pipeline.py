5279ad67144cc54ac950361607e24c72a82c2194
ivansg44 <ivansg44@gmail.com>
2018-07-17 14:09:28 -0700

    Implemented handling of full-term matches with cleaned sample and change of resource through find_full_term_match.

--- lexmapr/pipeline.py
+++ lexmapr/pipeline.py
@@ -661,7 +661,7 @@ def run(args):
                 # Update status_addendum
                 status_addendum.append(
                     "Permutation of Tokens in Bracketed Resource Term")
-            # Full-term match with cleaned sample
+            # Full-term cleaned sample match without any treatment
             elif cleaned_sample.lower() in resource_terms:
                 # Term with we found a full-term match for
                 matched_term = cleaned_sample.lower()
@@ -671,6 +671,17 @@ def run(args):
                 retained_tokens.append(matched_term + ":" + resource_id)
                 # Update status_addendum
                 status_addendum.append("A Direct Match with Cleaned Sample")
+            # Full-term cleaned sample match with change-of-case in
+            # resource data.
+            elif cleaned_sample.lower() in resource_terms_revised:
+                # Term with we found a full-term match for
+                matched_term = cleaned_sample.lower()
+                # Resource ID for matched_term
+                resource_id = resource_terms_revised[matched_term]
+                # Update retained_tokens
+                retained_tokens.append(matched_term + ":" + resource_id)
+                # Update status_addendum
+                status_addendum.append("Change of Case of Resource Terms")
             # Full-term match not found
             else:
                 # Find all suffixes that when appended to sample, are
@@ -698,7 +709,8 @@ def run(args):
                         + " to the Input]")
                 # No full-term match possible with suffixes either
                 else:
-                    raise MatchNotFoundError("Full-term match not found for: " + sample)
+                    raise MatchNotFoundError("Full-term match not found for: "
+                        + sample)
 
             # If we reach here, we had a full-term match with a
             # non-empty sample.
@@ -729,6 +741,7 @@ def run(args):
         # Rule2: Annotate all the Full Term Matches of Terms without any treatment
         # Rule3: Annotate all the Full Term Matches of Terms with change of case  -resourceRevisedTermsDict
         # Here we check all the suffices that can be applied to input term to make it comparable with resource terms
+        # Rule4: This will open now the cleaned sample to the test of Full Term Matching
         try:
             full_term_match = find_full_term_match(sample)
             if args.format == "full":
@@ -753,26 +766,7 @@ def run(args):
             logger.debug("==============" + sample.lower())
             logger.debug("--------------" + cleaned_sample.lower())
 
-            if ((cleaned_sample.lower() in resource_terms_revised.keys() ) and not trigger):
-                if (cleaned_sample.lower() in resource_terms_revised.keys()):
-                    resourceId = resource_terms_revised[cleaned_sample.lower()]  # Gets the id of the resource for matched term
-                status = "Full Term Match"
-                # statusAddendum = statusAddendum + "[Change of Case of Resource Terms]"
-                status_addendum.append("Change of Case of Resource Terms")
-                final_status = set(status_addendum)
-                retained_tokens.append(cleaned_sample.lower() + ":" + resourceId)
-                if args.format == 'full':
-                    fw.write('\t' + cleaned_sample.lower() + '\t' + str(list(retained_tokens)) + '\t' + str(list(retained_tokens)) + '\t' + '\t' + status + '\t' + str(list(final_status)))
-                else:
-                    fw.write('\t' + cleaned_sample.lower() + '\t' + str(list(retained_tokens)))
-                # To Count the Covered Tokens(words)
-                thisSampleTokens = word_tokenize(sample.lower())
-                for thisSampleIndvToken in thisSampleTokens:
-                    covered_tokens.append(thisSampleIndvToken)
-                    remaining_tokens.remove(thisSampleIndvToken)
-                trigger = True
-
-            elif (cleaned_sample.lower() in resource_permutation_terms.keys() and not trigger):
+            if (cleaned_sample.lower() in resource_permutation_terms.keys() and not trigger):
                 resourceId = resource_permutation_terms[cleaned_sample.lower()]
                 status = "Full Term Match"
                 # statusAddendum = statusAddendum + "[Permutation of Tokens in Resource Term]"
