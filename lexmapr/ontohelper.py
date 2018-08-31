ddfabda3fccdd2b859b76886b29a7d5b21bd5b30
ivansg44 <ivansg44@gmail.com>
2018-08-31 16:28:22 -0700

    do_output_json should now creates file in lexmapr.fetched_ontologies during integration testing.

--- lexmapr/ontohelper.py
+++ lexmapr/ontohelper.py
@@ -470,8 +470,7 @@ class OntoHelper(object):
 
 
 	def do_output_json(self, struct, output_file_basename):
-		print(os.listdir(lexmapr.pipeline.get_path("")))
-		with (open(output_file_basename + '.json', 'w')) as output_handle:
+		with (open(lexmapr.pipeline.get_path(output_file_basename + '.json'), 'w')) as output_handle:
 			# DO NOT USE sort_keys=True on piclists etc. because this overrides
 			# OrderedDict() sort order.
 			output_handle.write(json.dumps(struct, sort_keys = False, indent = 4, separators = (',', ': ')))
