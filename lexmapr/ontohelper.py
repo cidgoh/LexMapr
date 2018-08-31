c3c4d12bb84fbe0fc6e9a6392215da76810e2e1a
ivansg44 <ivansg44@gmail.com>
2018-08-31 16:14:23 -0700

    Made temporary print statement to determine why integration testing is failing.

--- lexmapr/ontohelper.py
+++ lexmapr/ontohelper.py
@@ -474,7 +474,7 @@ class OntoHelper(object):
 			# DO NOT USE sort_keys=True on piclists etc. because this overrides
 			# OrderedDict() sort order.
 			output_handle.write(json.dumps(struct, sort_keys = False, indent = 4, separators = (',', ': ')))
-			print(os.listdir(lexmapr.pipeline.get_path("fetched_ontologies")))
+			print(os.listdir(lexmapr.pipeline.get_path("lexmapr")))
 
 
 	def do_output_tsv(self, struct, output_file_basename, fields):
